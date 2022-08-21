0f5457301bbbed43029aae23119f9ffdd811abfb
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-21 10:08:28 +0530

    Create Q9.cpp

---
+++ Q9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cout<<"Enter x\t";cin>>x;
+    cout<<"Enter y\t";cin>>y;
+    if(x>y) cout<<x;
+    else cout<<y;
+
+    return 0;
+}
