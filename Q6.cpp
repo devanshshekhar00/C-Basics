f3d8a66386a3010155ad3b5e683db40a0404d007
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-21 09:57:17 +0530

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y,z,q;
+    cout<<"Enter x\t";cin>>x;
+    cout<<"Enter y\t";cin>>y;
+    cout<<"Enter z\t";cin>>z;
+    q = x+y+z;
+    
+    cout<<q/3;
+
+    return 0;
+}
